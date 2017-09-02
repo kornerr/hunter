c0f9d1b8676a8125f42e8a5f273e7c2334b49e1f
Michael Kapelko <kornerr@gmail.com>
2017-09-02 23:15:30 +0700

    Reference applib-nodep 0.0.2

--- cmake/projects/applib-nodep/hunter.cmake
+++ cmake/projects/applib-nodep/hunter.cmake
@@ -6,9 +6,15 @@ INCLUDE(hunter_pick_scheme)
 
 hunter_add_version(
     PACKAGE_NAME applib-nodep
-    VERSION "0.0.1"
-    URL "https://bitbucket.org/kornerr/hunter-applib-nodep/get/0.0.1.tar.gz"
+    VERSION 0.0.1
+    URL https://bitbucket.org/kornerr/hunter-applib-nodep/get/0.0.1.tar.gz
     SHA1 a8b4279df20a22e2cf04bfb43c456d1eea2b29de
+)
+hunter_add_version(
+    PACKAGE_NAME applib-nodep
+    VERSION 0.0.2
+    URL https://bitbucket.org/kornerr/hunter-applib-nodep/get/0.0.2.tar.gz
+    SHA1 3411b2bb043a0641e05aa1ae1343f461b4118804
 )
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
